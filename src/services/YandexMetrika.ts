54905f579a3f1ca77bff4260d188c0e7858265c8
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-06-24 18:02:14 +0300

    v0.1.2 - Fix build

--- src/services/YandexMetrika.ts
+++ src/services/YandexMetrika.ts
@@ -33,4 +33,101 @@ class YandexMetrika {
   }
 }
 
+interface YandexMetrikaFunction {
+  (
+    counterId: string,
+    event: 'init',
+    options?: {
+      accurateTrackBounce?: boolean;
+      childIframe?: boolean;
+      clickmap?: boolean;
+      ecommerce?: boolean | string | Array<any>;
+      params?: object | Array<any>;
+      userParams?: object;
+      trackHash?: boolean;
+      trackLinks?: boolean;
+      trustedDomains?: Array<string>;
+      type?: number;
+      ut?: string;
+      webvisor?: boolean;
+      triggerEvent?: boolean;
+    }
+  ): void;
+  (
+    counterId: string,
+    event: 'addFileExtension',
+    extensions: string | Array<string>
+  ): void;
+  (
+    counterId: string,
+    event: 'extLink',
+    url: string,
+    options?: {
+      callback?: Function;
+      ctx?: object;
+      params?: { order_price?: number; currency: string };
+      title?: string;
+    }
+  ): void;
+  (
+    counterId: string,
+    event: 'file',
+    url: string,
+    options?: {
+      callback?: Function;
+      ctx?: object;
+      params?: { order_price?: number; currency: string };
+      referer?: string;
+      title?: string;
+    }
+  ): void;
+  (
+    counterId: string,
+    event: 'getClientID',
+    callback: (clientId: string) => void
+  ): void;
+  (
+    counterId: string,
+    event: 'hit',
+    url: string,
+    options?: {
+      callback?: Function;
+      ctx?: object;
+      params?: { order_price?: number; currency: string };
+      referer?: string;
+      title?: string;
+    }
+  ): void;
+  (
+    counterId: string,
+    event: 'notBounce',
+    options?: {
+      callback?: Function;
+      ctx?: object;
+    }
+  ): void;
+  (counterId: string, event: 'params', parameters: object | Array<any>): void;
+  (
+    counterId: string,
+    event: 'reachGoal',
+    target: string,
+    params?: object,
+    callback?: Function,
+    ctx?: object
+  ): void;
+  (counterId: string, event: 'replacePhones'): void;
+  (counterId: string, event: 'setUserID', userId: string): void;
+  (
+    counterId: string,
+    event: 'userParams',
+    parameters: Record<string, any>
+  ): void;
+}
+
+declare global {
+  interface Window {
+    ym?: YandexMetrikaFunction;
+  }
+}
+
 export default YandexMetrika;
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,4 +1,4 @@
-// const commonjs = require('@rollup/plugin-commonjs');
+const commonjs = require('@rollup/plugin-commonjs');
 const path = require('path');
 
 /**
@@ -7,34 +7,34 @@ const path = require('path');
  */
 function external(id) {
     const peerDependencies = ['react', 'next']
-    const dependencies = ['js-cookie', 'react-ga']
+    const externalDependencies = ['cookie'];
+    const dependencies = ['js-cookie', 'react-ga', 'lodash/round', 'lodash-es/round']
 
     if (dependencies.includes(id)) return false;
-    if (peerDependencies.includes(id)) return true;
+    if (peerDependencies.includes(id) || externalDependencies.includes(id)) return true;
 
     return !id.startsWith('.') && !path.isAbsolute(id);
 }
 
 module.exports = {
     rollup(config) {
-        // Delete the external config property.
-        // This essentially means we're allowing all packages to be bundled.
         /**
          * Sources:
          * https://github.com/formik/tsdx/blob/158ee9a69c824b71b62cf987fe943a167f47f936/src/createRollupConfig.ts#L55-L59
          * https://github.com/formik/tsdx/issues/6#issuecomment-593146533
          */
-        // config.external = (id) => {
-        //     if ('babel-plugin-transform-async-to-promises/helpers' === id) {
-        //         return false;
-        //     }
-        //     console.log(`ID: "${id}" : ${external(id)}`)
-        //     return external(id);
-        // }
+        config.external = (id) => {
+            if ('babel-plugin-transform-async-to-promises/helpers' === id) {
+                return false;
+            }
+            return external(id);
+        }
 
-        // Manually use the commonjs plugin.
-        // This is opposed to specifying umd as the format as there's more implications that, again, are unclear.
-        // config.plugins.push(commonjs());
+        /**
+         * Manually use the commonjs plugin.
+         * This is opposed to specifying umd as the format as there's more implications that, again, are unclear.
+         */
+        config.plugins.push(commonjs());
 
         return config;
     },
