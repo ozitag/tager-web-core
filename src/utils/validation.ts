bfdc37e9f4589669839470336fd0e8b138193d03
Vital Ozierski <v.ozierski@ozitag.com>
2021-11-29 21:51:22 +0300

    0.3.24 - add simple errors handling support to convertRequestErrorToMap

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -6,44 +6,62 @@ import RequestError from '../services/RequestError';
  * https://html.spec.whatwg.org/multipage/input.html#e-mail-state-(type=email)
  */
 export const EMAIL_REGEXP =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 export const validators = {
-  required(value: string): boolean {
-    return !value.trim();
-  },
-  email(value: string): boolean {
-    return !EMAIL_REGEXP.test(value);
-  },
+    required(value: string): boolean {
+        return !value.trim();
+    },
+    email(value: string): boolean {
+        return !EMAIL_REGEXP.test(value);
+    },
 };
 
 const requestErrorBodyContract = t.object({
-  errors: t.objectOf(
-    t.object({
-      code: t.string,
-      message: t.string,
-    })
-  ),
+    errors: t.objectOf(
+        t.object({
+            code: t.string,
+            message: t.string,
+        })
+    ),
+});
+
+const requestErrorBodySimpleContract = t.object({
+    errors: t.objectOf(t.string)
 });
 
 export function convertRequestErrorToMap(error: Error): Record<string, string> {
-  if (error instanceof RequestError) {
-    const validationResult = requestErrorBodyContract(
-      'RequestError.body',
-      error.body
-    );
+    if (error instanceof RequestError) {
+        if (!error.body) {
+            return {};
+        }
 
-    if (validationResult instanceof t.ValidationError) {
-      return {};
-    } else {
-      return Object.keys(validationResult.errors).reduce<
-        Record<string, string>
-      >((result, key) => {
-        result[key] = validationResult.errors[key]?.message || '';
-        return result;
-      }, {});
+        const validationResult = requestErrorBodyContract(
+            'RequestError.body',
+            error.body
+        );
+
+        if (!(validationResult instanceof t.ValidationError)) {
+            return Object.keys(validationResult.errors).reduce<Record<string, string>>((result, key) => {
+                result[key] = validationResult.errors[key]?.message || '';
+                return result;
+            }, {});
+        }
+
+        const validationSimpleResult = requestErrorBodySimpleContract(
+            'RequestError.body',
+            error.body
+        );
+
+        if (!(validationSimpleResult instanceof t.ValidationError)) {
+            return Object.keys(validationSimpleResult.errors).reduce<Record<string, string>>((result, key) => {
+                result[key] = validationSimpleResult.errors[key] || '';
+                return result;
+            }, {});
+        }
+
+        return {};
     }
-  }
 
-  return {};
+    return {};
 }
